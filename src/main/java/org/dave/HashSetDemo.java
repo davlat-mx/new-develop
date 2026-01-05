ad36b7d9d315edf2df998d85511915aed7d2a2e2
sansiok <muxtorovdavlatbek@gmail.com>
2026-01-05 18:35:57 +0500

    hashset

---
+++ src/main/java/org/dave/HashSetDemo.java
@@ -0,0 +1,18 @@
+package org.dave;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        HashSet<String> hs = new LinkedHashSet<>();
+//        HashSet<String> hs = new HashSet<>();
+        hs.add ("Beta ");
+        hs.add ("Alpha ");
+        hs.add ("Eta");
+        hs.add ("Gamma ");
+        hs.add ("Epsilon ");
+        hs.add ("Omega ");
+        System.out.println(hs);
+    }
+}
